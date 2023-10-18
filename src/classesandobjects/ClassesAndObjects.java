ea121cdda9c14fd00ff73ad94fa204d4c4f689d2
2022281 <2022281@student.cct.ie>
2023-10-18 23:52:57 +0100

    Added an import statement for the new shapes package and triangle class.

--- src/classesandobjects/ClassesAndObjects.java
+++ src/classesandobjects/ClassesAndObjects.java
@@ -4,6 +4,8 @@
  */
 package classesandobjects;
 
+import shapes.Triangle;
+
 /**
  *
  * @author aldol
@@ -15,7 +17,36 @@ public class ClassesAndObjects {
      */
     public static void main(String[] args) {
         
+        Triangle tri1 = new Triangle();
+        Triangle tri2 = new Triangle();
+        Triangle tri3 = new Triangle();
+        Triangle tri4 = new Triangle();
+        Triangle tri5 = new Triangle();
+        Triangle tri6 = new Triangle();
         
+        //the lengths of tril1`s sides will be first, then the lengths of tril2`s etc.
+        int [] sideLengths = new int[18]; //3x6=18
+        
+        sideLengths[0] = 5;
+        sideLengths[1] = 8;
+        sideLengths[2] = 9;
+        
+        //I want to specifie that triagle 5 has the sideLengths (4, 7, 2)
+        sideLengths[12] = 4;
+        sideLengths[13] = 7;
+        sideLengths[14] = 2;
+        
+        //we want to set the middle side of each triangle to be 3.
+        sideLengths[1] = 3;
+        sideLengths[4] = 3;
+        sideLengths[7] = 3;
+        sideLengths[10] = 3;
+        sideLengths[13] = 3;
+        sideLengths[16] = 3;
+        
+        
+        Person jose = new Person();
+        jose.favTriangle = tri5;
         
     }
     
---
+++ src/classesandobjects/Person.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classesandobjects;
+
+import shapes.Triangle;
+
+
+/**
+ *
+ * @author aldol
+ */
+public class Person { //I criated a class called Person and now I`m creating some atributes
+    int age;
+    String name;
+    double height; 
+    boolean isProgrammer;
+    Triangle favTriangle; //Favourite triangle
+    
+}
